643ec9b4914fbbef357f8ed0d94f9fdc60790b95
astyarn <astyarn@gmail.com>
2024-12-18 13:15:51 +0100

    Uploaded opgave 2

---
+++ Opgave2-H4-2024/Debug/makedep.mk
@@ -0,0 +1,14 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+ExternalInterrupt.c
+
+Lib\Display_Lib\ssd1306.c
+
+Lib\Display_Lib\twi.c
+
+Lib\UART_Lib\UART.c
+
+main.c
+
